0ded5122aa5a14b39c11021733dca37e7be2378e
petrusjt <jozsefpetrus99@gmail.com>
2022-09-24 15:13:58 +0200

    temporarily removed tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// TODO create tests
+/*
 test('renders Hello world text', () => {
   render(<App />);
   const linkElement = screen.getByText(/Hello world!/i);
   expect(linkElement).toBeInTheDocument();
 });
+*/
