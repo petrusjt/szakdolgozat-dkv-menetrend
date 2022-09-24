52c36c2fc6a31165d091165b302ad7e4dee9df1f
petrusjt <jozsefpetrus99@gmail.com>
2022-09-24 08:58:36 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen bg-green-600 flex justify-center items-center">
+      <h1 className="font-bold text-3xl">Hello world!</h1>
     </div>
   );
 }
