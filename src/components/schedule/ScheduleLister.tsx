c7d9784430a2e6a36bd8e121281c33c6b291b8ac
petrusjt <jozsefpetrus99@gmail.com>
2023-03-18 08:09:35 +0100

    added: show schedule for selected line

--- src/components/schedule/ScheduleLister.tsx
+++ src/components/schedule/ScheduleLister.tsx
@@ -4,37 +4,15 @@ import {useParams} from "react-router-dom";
 import React, {useState} from "react";
 import {useTranslation} from "react-i18next";
 import {FormControl, InputLabel, MenuItem, Select} from "@mui/material";
-
-/*
-type Props = {
-    selectedLine: number,
-    startingPoint: string
-}
-
-export function ScheduleLister(props: Props) {
-    const schedule = getScheduleForLine("" + props.selectedLine)
-    const startingPoint = schedule.stops.filter(stop => stop.name === props.startingPoint)[0] || schedule.stops[0]
-
-    return (
-        <div className="w-full mt-2 p-3 rounded-md">
-            <h1 className="font-bold">{props.selectedLine} megállók</h1>
-            {schedule.stops.map((item: Stop) =>
-                <div key={item.name} className="flex w-full justify-between">
-                    <div className={`${item.name === schedule.startsFrom.name ? 'underline' : ''}
-                                     ${item.name === startingPoint.name ? 'font-bold' : ''}`}>
-                        {item.name}
-                    </div>
-                    <div>{item.timeFromStart - startingPoint.timeFromStart}</div>
-                </div>
-            )}
-        </div>
-    )
-}*/
+import SwapHorizOutlinedIcon from '@mui/icons-material/SwapHorizOutlined';
+import {Schedule} from "src/model/schedule/schedule";
+import {StartTimes} from "src/model/schedule/start-times";
 
 export function ScheduleLister() {
     const [t,] = useTranslation()
     const params = useParams()
     const scheduleInfo = getScheduleForLine(params["lineId"] as string)
+    const [scheduleAtSelectedStop, setScheduleAtSelectedStop] = useState(scheduleInfo)
     const [startingPointName, setStartingPointName] = useState(params["startFrom"] || "")
     const [startingPoint, setStartingPoint] = useState(params["startFrom"]
         ? scheduleInfo.stops.filter(item => item.name === params["startFrom"])[0]
@@ -42,37 +20,46 @@ export function ScheduleLister() {
 
     const handleStartingPointSelect = (event: any) => {
         const stop = scheduleInfo.stops.filter(item => item.name === event.target.value)[0]
-        console.log(stop)
         setStartingPoint(stop)
         setStartingPointName(event.target.value)
+        setScheduleAtSelectedStop(transformScheduleByStartingPoint(scheduleInfo, stop))
     }
 
     return (
         <>
+            {/* TODO: kiírni a vonal(ak) nevét és indulás/végállomást (több esetén több sorban?) */}
             <FormControl className="card w-full" size="small">
-                <InputLabel id="startingPointSelectLabel" className="mui-color-override">
-                    {t("schedule.selector.startingPoint").toString()}
-                </InputLabel>
-                <Select labelId="startingPointSelectLabel"
-                        label={t("schedule.selector.startingPoint").toString()}
-                        value={startingPointName}
-                        sx={{
-                            color: 'black',
-                            '& .MuiOutlinedInput-notchedOutline': {
-                                borderColor: '#999'
-                            },
-                            '& .MuiSvgIcon-root': {
-                                color: '#999'
+                <div className="flex w-full gap-1">
+                    <div className="w-11/12">
+                        <InputLabel id="startingPointSelectLabel" className="mui-color-override">
+                            {t("schedule.selector.startingPoint").toString()}
+                        </InputLabel>
+                        <Select labelId="startingPointSelectLabel"
+                                label={t("schedule.selector.startingPoint").toString()}
+                                value={startingPointName}
+                                sx={{
+                                    color: 'black',
+                                    '& .MuiOutlinedInput-notchedOutline': {
+                                        borderColor: '#999'
+                                    },
+                                    '& .MuiSvgIcon-root': {
+                                        color: '#999'
+                                    }
+                                }}
+                                className="dark:!text-white w-full"
+                                onChange={handleStartingPointSelect}>
+                            {
+                                // TODO villamosoknál ez problémás lesz
+                                scheduleInfo.stops.map((stop: Stop) =>
+                                    <MenuItem value={stop.name} key={stop.name}>{stop.name}</MenuItem>)
                             }
-                        }}
-                        className="dark:!text-white"
-                        onChange={handleStartingPointSelect}>
-                    {
-                        // TODO villamosoknál ez problémás lesz
-                        scheduleInfo.stops.map((stop: Stop) =>
-                            <MenuItem value={stop.name} key={stop.name}>{stop.name}</MenuItem>)
-                    }
-                </Select>
+                        </Select>
+                    </div>
+                    <div className="w-1/12 flex justify-center items-center dark:bg-slate-700 cursor-pointer rounded-md
+                                    dark:border-gray-300 border-gray-400 border-[1px]">
+                        <SwapHorizOutlinedIcon className="mui-color-override" />
+                    </div>
+                </div>
             </FormControl>
             <div className="card w-full mt-2 p-3 rounded-md">
                 <h1 className="font-bold">{t("line.stops").replace("%s", params["lineId"] as string)}</h1>
@@ -86,6 +73,35 @@ export function ScheduleLister() {
                     </div>
                 )}
             </div>
+            <div className="card w-full mt-2 p-3 rounded-md">
+                {scheduleAtSelectedStop.startTimes.map(startTime =>
+                    <div className="w-full border-b-[1px] border-gray-300 mt-2 flex gap-1">
+                        <div className="pr-1 border-gray-300 border-r-2">{startTime.hour.toString()}</div>
+                        <div className="pl-1 flex justify-start gap-1.5">
+                            {startTime.minutes.map(minute =>
+                                <span key={minute}>{minute}</span>
+                            )}
+                        </div>
+                    </div>
+                )}
+            </div>
         </>
     )
+}
+
+function transformScheduleByStartingPoint(schedule: Schedule, startingPoint: Stop): Schedule {
+    let startTimes: Array<StartTimes> = schedule.startTimes.map(startTime =>
+        new StartTimes(startTime.hour, startTime.minutes.map(minute => minute + startingPoint.timeFromStart)))
+    for (let hour = 0; hour < startTimes.length; hour++) {
+        const minutesOver59 = startTimes[hour].minutes
+            .filter(minute => minute > 59)
+        for (let minute of minutesOver59) {
+            const hourRolloverCount = Math.floor(minute / 60)
+            const realMinute = minute % 60
+            startTimes[hour + hourRolloverCount].minutes.push(realMinute)
+        }
+        startTimes[hour] = new StartTimes(startTimes[hour].hour, startTimes[hour].minutes
+            .filter(minute => minute < 60).sort())
+    }
+    return new Schedule(schedule.line, schedule.stops, startTimes, schedule.startsFrom)
 }
